6f7dea39489e708d516368782d3153e63a818182
Serge40in <serge40in@ovi.com>
2012-03-16 12:03:00 +0700

    Update features/support/paths.rb

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -16,6 +16,13 @@ module NavigationHelpers
     when /^the home\s?page$/
       '/'
 
+    when /^the RottenPotatoes home page/
+      '/movies'
+
+    when /^Create new movie page/
+      '/movies/new'
+      
+
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
